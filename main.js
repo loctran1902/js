99004557faddb33d32ad6522859521764cf834c1
loctran1902 <mrloctran19@gmail.com>
2023-06-14 13:24:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+function getScrollPercentage() {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight - windowHeight;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollPercentage = (scrollTop / documentHeight) * 100 + '%';
+    return scrollPercentage;
+  }
